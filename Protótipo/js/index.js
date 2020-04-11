076caaab67be44a7f1987618a81076e7b06a6857
NunoCosta13 <nunocos125@gmail.com>
2020-04-11 03:17:44 +0100

    mudado

--- Protótipo/js/index.js
+++ Protótipo/js/index.js
@@ -1,17 +1,19 @@
 (function() {
 
-    var preload = document.getElementById("preload");
-    var loading = 0;
-    var id = setInterval(frame, 64);
+    var preload = document.getElementById("preload")
+    var loading = 0
+    var id = setInterval(frame, 64)
+    var loaded = false
 
     function frame() {
-        if (loading == 100) {
+        if (loading == 100 && !loaded) {
             clearInterval(id);
-            window.open("./content/welcome.html", "_self");
+            window.open("./content/login.html", "_self")
+            loaded = true
         } else {
-            loading = loading + 1;
+            loading = loading + 1
             if (loading == 90) {
-                preload.style.animation = "fadeout 1s ease";
+                preload.style.animation = "fadeout 1s ease"
             }
         }
     }
